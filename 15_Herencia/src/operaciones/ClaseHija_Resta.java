a9dc9c67ef78f2f8a0a2cabeed06664bfc8db461
Marcelo Sobarzo <sobarzo.marcelo@gmail.com>
2022-09-02 15:33:09 -0400

    Add files via upload

---
+++ 15_Herencia/src/operaciones/ClaseHija_Resta.java
@@ -0,0 +1,11 @@
+package operaciones;
+
+
+public class ClaseHija_Resta extends ClasePadre{
+	
+	public void Restar() {
+		
+		resultado = valor1 - valor2 ;
+	}
+
+}
---
+++ 15_Herencia/src/operaciones/ClaseHija_Suma.java
@@ -0,0 +1,10 @@
+package operaciones;
+
+
+public class ClaseHija_Suma extends ClasePadre{
+	
+	public void Sumar() {
+		resultado = valor1 + valor2 ;
+	}
+
+}
