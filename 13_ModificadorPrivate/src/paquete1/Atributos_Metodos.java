ffbd0f8b77904ad95f1c0f23c420298932e78fb9
Marcelo Sobarzo <sobarzo.marcelo@gmail.com>
2022-09-02 15:32:15 -0400

    Add files via upload

---
+++ 13_ModificadorPrivate/src/paquete1/Atributos_Metodos.java
@@ -0,0 +1,38 @@
+package paquete1;
+
+public class Atributos_Metodos {
+
+	private int valorUno;
+	private int valorDos;
+	private String valorTres;
+
+	private void Metodomarcelo() {
+		
+	}
+
+	public int getValorUno() {
+		return valorUno;
+	}
+
+	public void setValorUno(int valorUno) {
+		this.valorUno = valorUno;
+	}
+
+	public int getValorDos() {
+		return valorDos;
+	}
+
+	public void setValorDos(int valorDos) {
+		this.valorDos = valorDos;
+	}
+
+	public String getValorTres() {
+		return valorTres;
+	}
+
+	public void setValorTres(String valorTres) {
+		this.valorTres = valorTres;
+	}
+	
+	
+}
